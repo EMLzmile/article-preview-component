6d351ad129bfe30d6081f9bb0121adfa08c687c5
EMLzmile <emmanuelmoolongawi@gmail.com>
2024-06-25 15:07:33 +0100

    update

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log('Hello World!');
+const share = document.querySelector('.icon-share');
+const iconShare = share.querySelector('img');
+
+let idTimeout = null;
+
+function shareMenu() {
+  share.classList.toggle('white');
+  
+  if (idTimeout !== null) {
+    clearTimeout(idTimeout);
+    idTimeout = null;
+    iconShare.src = './images/icon-share.svg';
+  } else {
+    idTimeout = setTimeout(shareMenu, 10000);
+    iconShare.src = './images/icon-share-white.svg';
+  }
+};
+
+share.onclick = shareMenu;
